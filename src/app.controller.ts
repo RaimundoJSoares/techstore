7d05069524edd0de0bb40666ab56be14c4280e3d
RaimundoJSoares <raianimes1000@gmail.com>
2022-11-28 09:07:29 -0400

    add typeorm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,26 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    // eslint-disable-next-line prefer-const
+    let viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['title'] = 'About Us - Online Store';
+    viewData['subtitle'] = 'About Us';
+    viewData['description'] = 'This is the about page...';
+    viewData['author'] = 'Developed by: Your name';
+
+    return {
+      viewData: viewData,
+    };
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
+import { ProductsController } from './products.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'techstore',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, ProductsController],
 })
 export class AppModule {}
